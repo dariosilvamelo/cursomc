6c885c08c2def490f5d8c9c19906635b2f9210e4
dariosilvamelo <dariosilvamelo@gmail.com>
2022-07-16 11:23:51 -0300

    Aula UML - vesão 02.00 : Pacote de Dominio: Criando a Classe Categoria.

--- src/main/java/com/dariosilvamelo/cursomc/resouces/CategoriaResouce.java
+++ src/main/java/com/dariosilvamelo/cursomc/resouces/CategoriaResouce.java
@@ -1,16 +1,29 @@
 package com.dariosilvamelo.cursomc.resouces;
 
+import java.util.ArrayList;
+import java.util.List;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.dariosilvamelo.cursomc.domain.Categoria;
 
 @RestController
 @RequestMapping(value = "/categorias")
 public class CategoriaResouce {
 	
     @RequestMapping(method=RequestMethod.GET)
-	public String listar() {
-    	return "Rest está funcionando";
+	public List<Categoria> listar() {
+    	
+    	Categoria c1 = new  Categoria(1,"Informartica");
+       	Categoria c2 = new  Categoria(2,"Escritorio");
+       	
+       	List<Categoria> list = new ArrayList<>();
+       	
+       	list.add(c1);
+       	list.add(c2);
+
+       	return list;
     }
 
 }

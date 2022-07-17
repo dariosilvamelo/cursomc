b2c28658056627467683fe8c89f1e5ad40739d26
dariosilvamelo <dariosilvamelo@gmail.com>
2022-07-17 15:04:28 -0300

    Aula UML - vesão 05.00 : Inserindo dados na tabela Categoria

--- src/main/java/com/dariosilvamelo/cursomc/CursomcApplication.java
+++ src/main/java/com/dariosilvamelo/cursomc/CursomcApplication.java
@@ -1,13 +1,33 @@
 package com.dariosilvamelo.cursomc;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.dariosilvamelo.cursomc.domain.Categoria;
+import com.dariosilvamelo.cursomc.repositores.CategoriaRepository;
+
 @SpringBootApplication
-public class CursomcApplication {
+public class CursomcApplication implements CommandLineRunner{
+	
+	@Autowired
+	private CategoriaRepository categoriaRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+	
+		Categoria cat01 = new Categoria(null,"Informatica");
+		Categoria cat02 = new Categoria(null,"Escritorio");
+		
+		categoriaRepository.saveAll(Arrays.asList(cat01, cat02));
+		 
 	}
 
 }

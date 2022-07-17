7573b5ffa042245b9a2d88aa6277c99c94ad2ef4
dariosilvamelo <dariosilvamelo@gmail.com>
2022-07-17 17:20:41 -0300

    Aula UML - versão 06.00 : Criando a associação das tabelas Categoria e Produto

--- src/main/java/com/dariosilvamelo/cursomc/CursomcApplication.java
+++ src/main/java/com/dariosilvamelo/cursomc/CursomcApplication.java
@@ -8,13 +8,18 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.dariosilvamelo.cursomc.domain.Categoria;
+import com.dariosilvamelo.cursomc.domain.Produto;
 import com.dariosilvamelo.cursomc.repositores.CategoriaRepository;
+import com.dariosilvamelo.cursomc.repositores.ProdutoRepository;
 
 @SpringBootApplication
 public class CursomcApplication implements CommandLineRunner{
 	
 	@Autowired
 	private CategoriaRepository categoriaRepository;
+	
+	@Autowired
+	private ProdutoRepository produtoRepository;	
 
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
@@ -22,11 +27,26 @@ public class CursomcApplication implements CommandLineRunner{
 
 	@Override
 	public void run(String... args) throws Exception {
+		
+		
 	
 		Categoria cat01 = new Categoria(null,"Informatica");
 		Categoria cat02 = new Categoria(null,"Escritorio");
 		
+		Produto p1 = new Produto(null,"Computador",2000.00);
+		Produto p2 = new Produto(null,"Impressora",800.00);		
+		Produto p3 = new Produto(null,"Mouse",2000.00);
+		
+		
+		cat01.getProdutos().addAll(Arrays.asList(p1,p2,p3));
+		cat02.getProdutos().addAll(Arrays.asList(p2));
+		
+		p1.getCategorias().addAll(Arrays.asList(cat01));
+		p2.getCategorias().addAll(Arrays.asList(cat01,cat02));
+		p3.getCategorias().addAll(Arrays.asList(cat01));
+
 		categoriaRepository.saveAll(Arrays.asList(cat01, cat02));
+		produtoRepository.saveAll(Arrays.asList(p1,p2,p3));
 		 
 	}
 
--- src/main/java/com/dariosilvamelo/cursomc/domain/Produto.java
+++ src/main/java/com/dariosilvamelo/cursomc/domain/Produto.java
@@ -9,44 +9,42 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
 
 
 @Entity
-public class Categoria implements Serializable{
+public class Produto implements Serializable{
 	
 	private static final long serialVersionUID = 1L;
 	
 	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@GeneratedValue(strategy = GenerationType.IDENTITY)	
 	private Integer id;
-	private String nome;
+	private String  nome;
+	private Double  preço;
 	
 	
 	
+	@ManyToMany
+	@JoinTable ( name               = "Produto_Categoria",
+	             joinColumns        = @JoinColumn(name  ="produto_id"   ),
+	             inverseJoinColumns = @JoinColumn(name  = "categoria_id") 
+	           )
+	private List<Categoria> categorias = new ArrayList<>();
 	
-	@ManyToMany (mappedBy = "categorias")
-	private List<Produto> produtos = new ArrayList<>();
 	
-	
-	public Categoria() {
+	public Produto() {
 		
 	}
 
-	public Categoria(Integer id, String nome) {
+	public Produto(Integer id, String nome, Double preço) {
 		super();
 		this.id = id;
 		this.nome = nome;
+		this.preço = preço;
 	}
-	
-	public List<Produto> getProdutos() {
-		return produtos;
-	}
-
-	public void setProdutos(List<Produto> produtos) {
-		this.produtos = produtos;
-	}
-	
 
 	public Integer getId() {
 		return id;
@@ -64,6 +62,22 @@ public class Categoria implements Serializable{
 		this.nome = nome;
 	}
 
+	public Double getPreço() {
+		return preço;
+	}
+
+	public void setPreço(Double preço) {
+		this.preço = preço;
+	}
+
+	public List<Categoria> getCategorias() {
+		return categorias;
+	}
+
+	public void setCategorias(List<Categoria> categorias) {
+		this.categorias = categorias;
+	}
+
 	@Override
 	public int hashCode() {
 		return Objects.hash(id);
@@ -77,9 +91,7 @@ public class Categoria implements Serializable{
 			return false;
 		if (getClass() != obj.getClass())
 			return false;
-		Categoria other = (Categoria) obj;
+		Produto other = (Produto) obj;
 		return Objects.equals(id, other.id);
 	}
-
-
 }

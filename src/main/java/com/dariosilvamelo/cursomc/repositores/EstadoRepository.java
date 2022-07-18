f50c00b01422a10925989754b75d37838ca6eae4
dariosilvamelo <dariosilvamelo@gmail.com>
2022-07-17 22:19:49 -0300

    Aula UML - versão 08.00 : Criação da tabela Estado e Cidade e inserindo dados

---
+++ src/main/java/com/dariosilvamelo/cursomc/repositores/EstadoRepository.java
@@ -0,0 +1,16 @@
+package com.dariosilvamelo.cursomc.repositores;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+
+
+import org.springframework.stereotype.Repository;
+
+
+import com.dariosilvamelo.cursomc.domain.Estado;
+
+
+@Repository
+public interface EstadoRepository extends JpaRepository<Estado, Integer> {
+
+}
